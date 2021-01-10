c1b8400a89a684f2a7bc85d4d620e1071d808003
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2021-01-10 22:40:14 +0530

    Add files via upload

---
+++ 4. User Input with Text Field.py
@@ -0,0 +1,32 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+# from kivymd.uix.textfield import MDTextField
+from kivy.lang import Builder
+
+username_helper = """
+MDTextField:
+    hint_text: "Enter Username"
+    helper_text: "Or Click on Forgot Username"
+    helper_text_mode: "on_focus"
+    icon_right: "android"
+    icon_attribute: "app.theme_cls.primary_color"
+    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x: None
+    width: 300
+"""
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Green"
+        screen = Screen()
+        #    username = MDTextField(text='Enter Username',
+        #                           pos_hint={'center_x': 0.5, 'center_y': 0.5},
+        #                           size_hint_x=None, width=300)
+
+        username = Builder.load_string(username_helper)
+        screen.add_widget(username)
+        return screen
+
+
+MainApp().run()
