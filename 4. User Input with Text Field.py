0291a357b0c7d8daefeb03586677c6813a5ac16d
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2021-01-11 01:42:11 +0530

    Update 4. User Input with Text Field.py

--- 4. User Input with Text Field.py
+++ 4. User Input with Text Field.py
@@ -1,6 +1,5 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-# from kivymd.uix.textfield import MDTextField
 from kivy.lang import Builder
 
 username_helper = """
@@ -9,7 +8,7 @@ MDTextField:
     helper_text: "Or Click on Forgot Username"
     helper_text_mode: "on_focus"
     icon_right: "android"
-    icon_attribute: "app.theme_cls.primary_color"
+    icon_right_color: app.theme_cls.primary_color
     pos_hint: {'center_x': 0.5, 'center_y': 0.5}
     size_hint_x: None
     width: 300
@@ -20,10 +19,6 @@ class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Green"
         screen = Screen()
-        #    username = MDTextField(text='Enter Username',
-        #                           pos_hint={'center_x': 0.5, 'center_y': 0.5},
-        #                           size_hint_x=None, width=300)
-
         username = Builder.load_string(username_helper)
         screen.add_widget(username)
         return screen
